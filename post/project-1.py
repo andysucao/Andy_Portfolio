c4abbc33921cbb6f6511b350ae2a0c7319c08b04
andy <76654053+andysucao@users.noreply.github.com>
2022-07-11 10:58:40 -0400

    commit 1

---
+++ post/project-1.py
@@ -0,0 +1,56 @@
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.io as pio
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
+df.head()
+
+df['text'] = df['name'] + '<br>Population ' + (df['pop']/1e6).astype(str)+' million'
+limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
+colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
+cities = []
+scale = 5000
+
+fig = go.Figure()
+
+for i in range(len(limits)):
+    lim = limits[i]
+    df_sub = df[lim[0]:lim[1]]
+    fig.add_trace(go.Scattergeo(
+        locationmode = 'USA-states',
+        lon = df_sub['lon'],
+        lat = df_sub['lat'],
+        text = df_sub['text'],
+        marker = dict(
+            size = df_sub['pop']/scale,
+            color = colors[i],
+            line_color='rgb(40,40,40)',
+            line_width=0.5,
+            sizemode = 'area'
+        ),
+        name = '{0} - {1}'.format(lim[0],lim[1])))
+
+fig.update_layout(
+        title_text = '2014 US city populations<br>(Click legend to toggle traces)',
+        showlegend = True,
+        geo = dict(
+            scope = 'usa',
+            landcolor = 'rgb(217, 217, 217)',
+        )
+    )
+
+fig.show()
+
+
+
+fig.write_html("project-1-1.html")
+
+fig.write_image("project-1-2.svg")
+
+fig.write_image("project-1-3.pdf")
+
+fig.write_image("project-1-4.png")
+
+pio.write_image(fig, 'project-1-5.png', width=1200, height=1200)
+
